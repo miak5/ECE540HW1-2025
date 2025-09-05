35ccb891c8d0eb59177f40f2eab326259f0eac2e
miak5 <miak5@ksu.edu>
2025-09-05 11:46:20 -0500

    Update FinalPrecision.m

--- FinalPrecision.m
+++ FinalPrecision.m
@@ -1,5 +1,11 @@
 function FinalPrecision()
-%this 
+%My function FinalPrecision calculates the precision values for decreasing base values. 
+%In our assignment we start the base at 1 and then decrease it until its 1-e30
+%At the end of the function the results are stored into a CSV file and are plotted. 
+
+%Input- none
+%Output- plots and results written in CSV file
+%Purpose- Shows how precision changes with base values
 
 
 BaseValue = 1; %intialize base value
@@ -8,7 +14,7 @@ ResultsMatrix = []; %initializing results matrix
 PrecisionValue = PrecisionVersusBase(BaseValue);
 %file = fopen ('precision_results.csv','w');
 
-%loop base until its below 1e-30
+%loop base until its 1e-30
 while BaseValue >= 1e-30
     PrecisionValue = PrecisionVersusBase(BaseValue); %calling previous function for precision value
     RowMatrix = [BaseValue, PrecisionValue, PrecisionValue / BaseValue]; %Store results in matrix
@@ -28,3 +34,4 @@ set(gca, 'XDir', 'reverse'); %Reverse Direction of X axis
 figure;
 loglog(ResultsMatrix(:, 1 ), ResultsMatrix(:, 3 )), xlabel('Base'), ylabel('Ratio'), title('Base vs Ratio');
 set(gca, 'XDir', 'reverse'); %Reverse Direction of X axis  
+
